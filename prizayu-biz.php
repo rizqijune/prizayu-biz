dacdc36dd4fe577bbe07701e04bbc612dec96c46
rizqijune <cancerrizqi@gmai.com>
2024-11-23 21:54:47 +0700

    Minor enhancement

--- prizayu-biz.php
+++ prizayu-biz.php
@@ -4,8 +4,27 @@ namespace Grav\Theme;
 use Grav\Common\Theme;
 use Grav\Common\User;
 use Grav\Common\Grav;
+use Grav\Common\Data;
+use RocketTheme\Toolbox\Event\Event as EventInterface;
 
 class PrizayuBiz extends Theme
 {
-    
+    // Add author in page option
+    public function onBlueprintCreated(EventInterface $event)
+    {
+        static $inEvent = false;
+        $blueprint = $event['blueprint'];
+        $form = $blueprint->form();
+        $advanced_tab_exists = isset($form['fields']['tabs']['fields']['advanced']);
+        
+        if (!$inEvent && $advanced_tab_exists) {
+            $inEvent = true;
+            $blueprints = new Data\Blueprints(__DIR__ . '/blueprints/');
+            $extends = $blueprints->get('addons');
+            
+            $blueprint->extend($extends, true);
+            $inEvent = false;
+        }
+    }
+
 }
